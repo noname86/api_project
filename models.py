edc4e0d4d42b6ce82615ddc93f949ea29492a055
noname86 <bardifernando@gmail.com>
2023-10-19 20:03:53 +0100

    created Person Class

--- models.py
+++ models.py
@@ -1,2 +1,11 @@
 from datetime import datetime
 from config import db
+
+class Person(db.Model):
+    __tablename__ = "person"
+    id = db.Column(db.Intreger, primary_key=True)
+    lname = db.Column(db.String(32), unique=True)
+    fname = db.Column(db.String(32))
+    timestamp = db.Column(
+        db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow
+    )
