200886e3fd4fd86162f4277d3a14ea731fd05017
noname86 <bardifernando@gmail.com>
2023-10-21 12:38:38 +0100

    added Note class and cerated db relationship

--- models.py
+++ models.py
@@ -1,6 +1,17 @@
 from datetime import datetime
 from config import db, ma
 
+
+
+class Note(db.Model):
+    __tablename__ = "note"
+    id = db.Column(db.Integer, primary_key=True)
+    person_id = db.Column(db.Integer, db.ForeignKey("person.id"))
+    content = db.Column(db.String, nullable=False)
+    timestamp = db.Column(
+        db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow
+      )
+
 class Person(db.Model):
     __tablename__ = "person"
     id = db.Column(db.Integer, primary_key=True)
@@ -8,6 +19,13 @@ class Person(db.Model):
     fname = db.Column(db.String(32))
     timestamp = db.Column(
         db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow
+    )
+    notes = db.relationship(
+        Note,
+        backref="person",
+        cascade="all, delete, delete-orphan",
+        single_parent=True,
+        order_by="desc(Note.timestamp)"
     )
 
 class PersonSchema(ma.SQLAlchemyAutoSchema):
@@ -19,3 +37,5 @@ class PersonSchema(ma.SQLAlchemyAutoSchema):
 
 person_schema = PersonSchema()
 people_schema = PersonSchema(many=True)            
+
+
